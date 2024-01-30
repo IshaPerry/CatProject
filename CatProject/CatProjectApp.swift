493b5cad5f0d529f31539837c0f3ec0d31032113
IshaPerry <ishaperry@gmail.com>
2024-01-30 15:02:09 -0500

    Initial Commit

---
+++ CatProject/CatProjectApp.swift
@@ -0,0 +1,32 @@
+//
+//  CatProjectApp.swift
+//  CatProject
+//
+//  Created by Isha Perry on 1/30/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CatProjectApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ CatProject/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  CatProject
+//
+//  Created by Isha Perry on 1/30/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
